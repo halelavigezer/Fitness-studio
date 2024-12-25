30dd7802873a60621c7a3936daa80c3dc2344a63
EmunaSofer <emuna002@gmail.com>
2024-12-25 13:33:55 +0200

    fgf
    yes

--- gym/management/Gym.java
+++ gym/management/Gym.java
@@ -60,7 +60,8 @@ public class Gym {
         for (Instructor instructor : secretary.instructors) {
             ans += ("ID: "+instructor.getPerson().getID()+" | Name: "+instructor.getPerson().getName()+" | Gender: "+instructor.getPerson().getGender()+" | Birthday: "+instructor.getPerson().getDateOfBirth()+" | Age: "+secretary.getAge(instructor.getPerson())+" | Balance: "+instructor.getPerson().getMany()+" | Role: Instructor | Salary per Hour: "+instructor.getSalary()+" | Certified Classes: "+secretary.Mess(instructor.getTutorials())) + "\n"; // הוספת כל עובד לשורה נפרדת
         }
-        ans += ("ID: "+secretary.getPerson().getID()+" | Name: "+secretary.getPerson().getName()+" | Gender: "+secretary.getPerson().getGender()+" | Birthday: "+secretary.getPerson().getDateOfBirth()+" | Age: "+secretary.getAge(secretary.getPerson())+" | Balance: "+secretary.getPerson().getMany()+" | Role: Secretary | Salary per Month: "+secretary.many)+"\n\n";
+        ans += ("ID: "+secretary.getPerson().getID()+" | Name: "+secretary.getPerson().getName()+" | Gender: "+secretary.getPerson().getGender()+" | Birthday: "+secretary.getPerson().getDateOfBirth()+" | Age: "+secretary.getAge(secretary.getPerson())+" | Balance: "+secretary.getPerson().getMany()+" | Role: Secretary | Salary per Month: "+secretary.many)+"\n";
+        ans += "\nSessions Data:\n";
         for (Session session : secretary.sessions) {
             ans +=("Session Type:" +session.gettype()+" | Date: "+session.date+" | Forum: "+session.forumType+" | Instructor: "+session.instructor.getPerson().getName()+" | Participants: "+session.clients.size()+"/"+session.GetNumber())+ "\n"; // הוספת כל סשן לשורה נפרדת
         }
